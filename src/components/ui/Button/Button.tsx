4c23dd6c41d637c08755ae2091355afa84ca6b6e
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-05-15 00:57:20 +0300

    button fix

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,5 +1,23 @@
 import css from './Button.module.css';
 
-export default function Button({ text }: { text: string }) {
-  return <button className={css.button}>{text}</button>;
+interface ButtonProps {
+  href: string;
+  text: string;
+  padding: number[];
+  buttonVariant?: 'primaryButton' | 'secondaryButton';
+}
+
+export default function Button({
+  href,
+  text,
+  padding = [],
+  buttonVariant = 'primaryButton',
+}: ButtonProps) {
+  const style = css[buttonVariant];
+  const paddingStyle = padding.map((p) => `${p}px`).join(' ');
+  return (
+    <button type="button" className={style} style={{ padding: paddingStyle }}>
+      {text}
+    </button>
+  );
 }
