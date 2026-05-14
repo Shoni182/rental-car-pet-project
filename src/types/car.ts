ae3c65d5d3f97cbfcf9980dee1a45ac11e374199
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-05-14 16:15:23 +0300

    add car types

--- src/types/car.ts
+++ src/types/car.ts
@@ -1,2 +1,46 @@
-// Car
-// FilterTYpes
+export type Car = {
+  id: string;
+  year: number;
+  brand: string;
+  model: string;
+  type: string;
+  img: string;
+  description: string;
+  fuelConsumption: string;
+  engineSize: string;
+  accessories: string[];
+  functionalities: string[];
+  rentalPrice: string;
+  rentalCompany: string;
+  address: string;
+  rentalConditions: string[];
+  mileage: number;
+};
+
+// Filter
+export type FilterTypes = {
+  brands: string[];
+  price: {
+    min: number;
+    max: number;
+  };
+};
+
+// Response
+export type CarResponse = {
+  cars: Car[];
+  totalCars: number;
+  page: number;
+  totalPages: number;
+};
+
+// Form
+export type RentalFormData = {
+  name: string;
+  email: string;
+  comment: string;
+};
+// Form Response
+export type BookingResponse = {
+  message: string;
+};
