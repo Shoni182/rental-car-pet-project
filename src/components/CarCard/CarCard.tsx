0a6e00986fa8f795e4fa8ccbadc773e9e8e3d4ad
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-05-15 17:07:04 +0300

    add CarCard component

---
+++ src/components/CarCard/CarCard.tsx
@@ -0,0 +1,68 @@
+import css from './CarCard.module.css';
+import Image from 'next/image';
+
+import Button from '../ui/Button/Button';
+import { Car } from '@/types/car';
+
+type CarCardProps = Pick<
+  Car,
+  | 'id'
+  | 'img'
+  | 'brand'
+  | 'model'
+  | 'year'
+  | 'rentalPrice'
+  | 'location'
+  | 'rentalCompany'
+  | 'type'
+  | 'mileage'
+>;
+
+export default function CarCard({
+  id,
+  img,
+  brand,
+  model,
+  year,
+  rentalPrice,
+  location,
+  rentalCompany,
+  type,
+  mileage,
+}: CarCardProps) {
+  return (
+    <div className={css.carContainer}>
+      <Image
+        src={img}
+        alt="car image"
+        className={css.image}
+        width={244}
+        height={268}
+      />
+      {/* Name and Price */}
+      <div className={css.titleContainer}>
+        <p className={css.title}>
+          {brand} <span className={css.model}>{model}</span>, {year}
+        </p>
+        <span className={css.price}>{rentalPrice}</span>
+      </div>
+
+      <div className={css.detailsContainer}>
+        <span className={css.carDetails}>{location.city}</span>
+        <span className={css.carDetails}>{location.country}</span>
+        <span className={css.carDetails}>{rentalCompany}</span>
+        <span className={css.carDetails}>{type}</span>
+        <span className={css.carDetails}>{mileage}km</span>
+
+        {/* {isMile && <span>{mileage}mile</span>} */}
+      </div>
+
+      <Button
+        path={`/catalog/${id}`}
+        text="Read more"
+        padding={[12, 99]}
+        buttonVariant="primaryButton"
+      />
+    </div>
+  );
+}
