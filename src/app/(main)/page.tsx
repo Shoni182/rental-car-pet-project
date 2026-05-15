409cfbdb5c9322b15588c9c10115b65e7006c484
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-05-15 11:07:06 +0300

    add home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,21 @@
+import Button from '@/components/ui/Button/Button';
+import css from './page.module.css';
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <section className={css.homeSection}>
+      <div className={css.homeContainer}>
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <h2 className={css.subTitle}>
+          Reliable and budget-friendly rentals for any journey
+        </h2>
+        <Button
+          href="/catalog"
+          text="View Catalog"
+          padding={[12, 99]}
+          buttonVariant="primaryButton"
+        />
+      </div>
+    </section>
   );
 }
