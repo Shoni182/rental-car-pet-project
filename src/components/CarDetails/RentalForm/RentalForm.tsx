f33ab634dc04a09ddab200560bb031999ccfbf61
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-05-18 17:02:05 +0300

    add RentalForm component

--- src/components/CarDetails/RentalForm/RentalForm.tsx
+++ src/components/CarDetails/RentalForm/RentalForm.tsx
@@ -1,7 +1,25 @@
+import css from './RentalForm.module.css';
+
 type RentalFormProps = {
   carId: string;
 };
 
 export default function RentalForm({ carId }: RentalFormProps) {
-  return <h1>RentalForm</h1>;
+  return (
+    <div className={css.container}>
+      <h3 className={css.title}>Book your car now</h3>
+      <p className={css.subtitle}>
+        Stay connected! We are always ready to help you.
+      </p>
+
+      <form className={css.form}>
+        <input className={css.input} type="text" placeholder="Name*" />
+        <input className={css.input} type="email" placeholder="Email*" />
+        <textarea className={css.textarea} placeholder="Comment" />
+        <button className={css.button} type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
